c4de94067b14641782549c209606bd4123a26c78
Thanh Pham <pano.tjoan@yahoo.com>
2021-11-20 05:27:41 +0700

    fix(): provider test

--- lib/test/zeebe.providers.spec.ts
+++ lib/test/zeebe.providers.spec.ts
@@ -6,16 +6,11 @@ import * as zeebeProviders from '../zeebe.providers';
 describe('Providers', () => {
   describe('createZbClientProvider', () => {
     it("should use top-level queue name if it's not specified in factory options", async () => {
-      const moduleOptions: ZeebeModuleOptions = { hostname: 'localhost', port: '26500' };
-      const factoryModuleOptions: ZeebeModuleOptions = { hostname: 'localhost', port: '26500', loglevel: 'DEBUG' };
+      const moduleOptions: ZeebeModuleOptions = { gatewayAddress: 'localhost:26500' };
       const provider = zeebeProviders.createZbClientProvider() as FactoryProvider<ZBClient>;
 
-      let zbClient = provider.useFactory(moduleOptions);
-      expect(zbClient.loglevel).toEqual('INFO');
-      await zbClient.close();
-
-      zbClient = provider.useFactory(factoryModuleOptions);
-      expect(zbClient.loglevel).toEqual('DEBUG');
+      let zbClient = await provider.useFactory(moduleOptions);
+      expect(zbClient.gatewayAddress).toEqual(moduleOptions.gatewayAddress);
       await zbClient.close();
     });
   });
